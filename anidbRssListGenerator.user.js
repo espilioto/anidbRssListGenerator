5d4b46ff46d848f57426495393988d23e1557c3c
espilioto <espilioto@gmail.com>
2021-12-12 03:24:44 +0200

    Ditched the deprecated way to copy stuff to the clipboard

--- anidbRssListGenerator.user.js
+++ anidbRssListGenerator.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name         AniDB torrent rss list generator
-// @namespace    anindbrss
-// @version      1.1
-// @description  Generates a delimited anime name list for you to optimally edit
-// @author       SouLSLayeR
-// @match        https://anidb.net/user/wishlist/*
-// @grant        none
-// @downloadURL  https://raw.githubusercontent.com/espilioto/anidbRssListGenerator/master/anidbRssListGenerator.user.js
-// @updateURL	   https://raw.githubusercontent.com/espilioto/anidbRssListGenerator/master/anidbRssListGenerator.user.js
-// @homepage	   https://github.com/espilioto/anidbRssListGenerator
+// @name          AniDB torrent rss list generator
+// @namespace     anindbrss
+// @version       1.2
+// @description   Generates a delimited anime name list for you to optimally edit
+// @author        SouLSLayeR
+// @match         https://anidb.net/user/wishlist/*
+// @grant         GM.setClipboard
+// @downloadURL   https://raw.githubusercontent.com/espilioto/anidbRssListGenerator/master/anidbRssListGenerator.user.js
+// @updateURL     https://raw.githubusercontent.com/espilioto/anidbRssListGenerator/master/anidbRssListGenerator.user.js
+// @homepage      https://github.com/espilioto/anidbRssListGenerator
 // ==/UserScript==
 
 (function () {
@@ -23,14 +23,13 @@
     qbtRegexString = animuTitles.map((x) => `(${x})`).join("|");
 
     document.querySelector("#animu-list-button").addEventListener("click", () => {
-        var tempTextbox = document.createElement("textarea");
-        document.body.appendChild(tempTextbox);
-        tempTextbox.value = `uTorrent format:\r\n${utorrentString}\r\n\r\nqBt format:\r\n${qbtRegexString}`;
-        tempTextbox.select();
-        document.execCommand("copy");
-        document.body.removeChild(tempTextbox);
-
+      try{
+        GM.setClipboard(`uTorrent format:\r\n${utorrentString}\r\n\r\nqBt format:\r\n${qbtRegexString}`);
         toastr.success("Copied to clipboard");
-      });
+      }
+      catch(ex) {
+        toastr.error("Error copying to clipboard", ex);
+      }
+    });
   }
 })();
